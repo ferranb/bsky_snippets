69dba4581f4e7b507bc5108a40ee18045b0a8b5b
ferranb <ferranb@gmail.com>
2024-11-18 01:26:06 +0100

    Update create_post_with_hashtag.py
    Support for Unicode characters within the string

--- create_post_with_hashtag.py
+++ create_post_with_hashtag.py
@@ -1,9 +1,19 @@
 """
-This Python code shows how to post on Bluesky using hashtags that can be linked.
+This Python code shows how to post on Bluesky using hashtags that can be linked. 
+The code handles Unicode characters within the string
 """
 
 import re
 from atproto import Client, models
+
+
+def idx_from_str_to_byte(text: str, idx: int) -> int:
+    idx_byte = 0
+    for i in range(0, idx):
+        c = text[i]
+        c_unicode = c.encode('utf-8')
+        idx_byte += len(c_unicode)
+    return idx_byte
 
 
 class BlueSky:
@@ -13,25 +23,20 @@ class BlueSky:
 
     def publish(self, text: str):
         facets: list[models.AppBskyRichtextFacet.Main] = []
-        hashtags = re.finditer(r'#\w+', text)
+        hashtags = re.finditer(r'#\w+', text, re.DOTALL)
         for match in hashtags:
-            byte_start = match.regs[0][0]
-            byte_end = match.regs[0][1]
-            tag = match.string[byte_start+1:byte_end]  # We have to remove the hash character
+            idx_start = match.regs[0][0]
+            idx_end = match.regs[0][1]
+            tag = match.string[idx_start+1:idx_end]  # We have to remove the hash character
             m = models.AppBskyRichtextFacet.Main(
                 features=[models.AppBskyRichtextFacet.Tag(
                     tag=tag
                 )],
                 index=models.AppBskyRichtextFacet.ByteSlice(
-                    byteStart=byte_start,
-                    byteEnd=byte_end
+                    byteStart=idx_from_str_to_byte(text, idx_start),
+                    byteEnd=idx_from_str_to_byte(text, idx_end),
                 )
             )
             facets.append(m)
         post = self.client.send_post(text=text, facets=facets)
         return post
-
-
-if __name__ == "__main__":
-    b = BlueSky(user='your_user_name.bsky.social', password='some_very_strong_password')
-    b.publish("A #post #with #some #tags")
